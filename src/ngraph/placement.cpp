ba4d9ee009be84188dabec98fdf22a6a1822f0b2
Yixing Lao <yixing.lao@intel.com>
2018-03-14 06:28:51 -0700

    passing all tests

--- src/ngraph/placement.cpp
+++ src/ngraph/placement.cpp
@@ -540,32 +540,11 @@ void cluster_util::node_consistency_check_pass(const vector<shared_ptr<Cluster>>
     }
 }
 
-// vector<shared_ptr<Cluster>> cluster_util::split_function_to_clusters(const shared_ptr<Function>& f)
-// {
-//     // Init node cluster, every cluster contains one node
-//     vector<shared_ptr<Cluster>> clusters = cluster_util::build_singleton_clusters(f);
-//     node_consistency_check_pass(clusters, f);
-
-//     // Run cluster optimization passes
-//     // TODO: Currently runs node_consistency_check_pass for safety
-//     //       We only need to run once at the end
-//     cluster_util::merge_adjacent_clusters_pass(clusters);
-//     cluster_util::node_consistency_check_pass(clusters, f);
-
-//     cluster_util::merge_disjoint_clusters_pass(clusters);
-//     cluster_util::node_consistency_check_pass(clusters, f);
-
-//     cluster_util::topological_sort_clusters_pass(clusters);
-//     cluster_util::node_consistency_check_pass(clusters, f);
-
-//     return clusters;
-// }
-
-static shared_ptr<Node> take_independent_node_with_placement_priority(
-    const unordered_map<Placement, list<shared_ptr<Node>>>& independent_nodes_by_placement,
+static Node* take_independent_node_with_placement_priority(
+    map<Placement, deque<Node*>>& independent_nodes_by_placement,
     Placement placement)
 {
-    shared_ptr<Node> selected_node = nullptr;
+    Node* selected_node = nullptr;
     if (independent_nodes_by_placement.find(placement) != independent_nodes_by_placement.end() &&
         independent_nodes_by_placement.at(placement).size() != 0)
     {
@@ -574,7 +553,7 @@ static shared_ptr<Node> take_independent_node_with_placement_priority(
     }
     else
     {
-        for (auto it : independent_nodes_by_placement)
+        for (auto& it : independent_nodes_by_placement)
         {
             if (it.second.size() > 0)
             {
@@ -586,31 +565,34 @@ static shared_ptr<Node> take_independent_node_with_placement_priority(
     return selected_node;
 }
 
-vector<shared_ptr<Cluster>> cluster_util::split_function_to_clusters(const shared_ptr<Function>& f)
+static vector<shared_ptr<Cluster>> split_function_to_clusters_v2(const shared_ptr<Function>& f)
 {
     // Topologically sort nodes by picking independent node with the same placement as the
     // previously picked node greedily
-    unordered_map<Placement, list<shared_ptr<Node>>> independent_nodes_by_placement;
-    unordered_map<Placement, size_t> node_dependency_count;
-    list<shared_ptr<ngraph::Node>> sorted_nodes;
-    Placement previous_placement = Placement::DEFAULT;
+    map<Placement, deque<Node*>> independent_nodes_by_placement;
+    unordered_map<Node*, size_t> node_dependency_count;
+    unordered_map<ngraph::Node*, shared_ptr<ngraph::Node>> node_map;
 
     for (shared_ptr<Node> node : f->get_ops())
     {
         size_t dependency_count = node->get_input_ops().size();
-        node_dependency_count[node] = dependency_count;
+        node_map[node.get()] = node;
+        node_dependency_count[node.get()] = dependency_count;
         if (dependency_count == 0)
         {
-            independent_nodes_by_placement[node->get_placement()].push_back(node);
+            independent_nodes_by_placement[node->get_placement()].push_back(node.get());
         }
     }
-    while (shared_ptr<Node> independent_node = take_independent_node_with_placement_priority(
-               independent_nodes_by_placement, previous_placement))
+
+    list<shared_ptr<Node>> sorted_nodes;
+    Placement previous_placement = Placement::DEFAULT;
+    while (Node* independent_node = take_independent_node_with_placement_priority(
+           independent_nodes_by_placement, previous_placement))
     {
         previous_placement = independent_node->get_placement();
-        sorted_nodes.push_back(previous_placement);
+        sorted_nodes.push_back(node_map.at(independent_node));
 
-        for (auto user_node : independent_node->users())
+        for (Node* user_node : independent_node->users())
         {
             node_dependency_count.at(user_node) -= 1;
             if (node_dependency_count.at(user_node) == 0)
@@ -628,10 +610,61 @@ vector<shared_ptr<Cluster>> cluster_util::split_function_to_clusters(const share
         Placement node_placement = node->get_placement();
         if (node_placement != previous_placement)
         {
-            clusters.push_back(Cluster());
+            clusters.push_back(make_shared<Cluster>());
         }
-        clusters.back().insert_node(node);
+        clusters.back()->insert_node(node);
+        previous_placement = node_placement;
     }
 
     return clusters;
+}
+
+static vector<shared_ptr<Cluster>> split_function_to_clusters_v1(const shared_ptr<Function>& f)
+{
+    // Init node cluster, every cluster contains one node
+    vector<shared_ptr<Cluster>> clusters = cluster_util::build_singleton_clusters(f);
+    cluster_util::node_consistency_check_pass(clusters, f);
+
+    // Run cluster optimization passes
+    // TODO: Currently runs node_consistency_check_pass for safety
+    //       We only need to run once at the end
+    cluster_util::merge_adjacent_clusters_pass(clusters);
+    cluster_util::node_consistency_check_pass(clusters, f);
+
+    cluster_util::merge_disjoint_clusters_pass(clusters);
+    cluster_util::node_consistency_check_pass(clusters, f);
+
+    cluster_util::topological_sort_clusters_pass(clusters);
+    cluster_util::node_consistency_check_pass(clusters, f);
+
+    return clusters;
+}
+
+vector<shared_ptr<Cluster>> cluster_util::split_function_to_clusters(const shared_ptr<Function>& f)
+{
+    auto clusters_v1 = split_function_to_clusters_v1(f);
+    NGRAPH_INFO << "V1";
+    for (auto cluster: clusters_v1)
+    {
+        NGRAPH_INFO << "[Cluster] " << cluster->get_name();
+        for (auto node: cluster->get_nodes())
+        {
+            NGRAPH_INFO << node->get_name() << " " << placement_to_string(node->get_placement());
+        }
+    }
+
+    NGRAPH_INFO;
+    NGRAPH_INFO << "V2";
+    auto clusters_v2 = split_function_to_clusters_v2(f);
+    for (auto cluster: clusters_v2)
+    {
+        NGRAPH_INFO << "[Cluster] " << cluster->get_name();
+        for (auto node: cluster->get_nodes())
+        {
+            NGRAPH_INFO << node->get_name() << " " << placement_to_string(node->get_placement());
+        }
+    }
+
+
+    return clusters_v2;
 }
